0f1f986afdbf7a883059de5d9440668afd27167e
hai <hoanghai@gmail.com>
2019-11-22 19:33:57 +0700

    exam

---
+++ Exam-WAD/EXAM-WAD/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EXAM_WAD.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitslnStock { get; set; }
+        public int UnitOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public int Discontinued { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
